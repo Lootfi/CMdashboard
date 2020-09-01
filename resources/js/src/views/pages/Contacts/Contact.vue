d30db4c7de32ff50500c9a6f92817dca09e709c7
Lootfi <lotfiholmes@hotmail.com>
2020-09-01 17:39:21 +0100

    ::hammer:: fix

--- resources/js/src/views/pages/Contacts/Contact.vue
+++ resources/js/src/views/pages/Contacts/Contact.vue
@@ -8,194 +8,211 @@
 ========================================================================================== -->
 
 <template>
-  <div id="page-user-view">
-    <vs-alert color="danger" title="Contact Not Found" :active.sync="user_not_found">
-      <span>Contact record with slug: {{ $route.params.slug }} not found.</span>
-      <span>
-        <span>Check</span>
-        <router-link :to="{ name: 'contacts' }" class="text-inherit underline">All Contacts</router-link>
-      </span>
-    </vs-alert>
+    <div id="page-user-view">
+        <vs-alert
+            color="danger"
+            title="Contact Not Found"
+            :active.sync="user_not_found"
+        >
+            <span
+                >Contact record with slug: {{ $route.params.slug }} not
+                found.</span
+            >
+            <span>
+                <span>Check</span>
+                <router-link
+                    :to="{ name: 'contacts' }"
+                    class="text-inherit underline"
+                    >All Contacts</router-link
+                >
+            </span>
+        </vs-alert>
 
-    <div id="user-data" v-if="contact">
-      <vx-card title="Contact" class="mb-base">
-        <!-- Avatar -->
-        <div class="vx-row">
-          <!-- Avatar Col -->
-          <div class="vx-col" id="avatar-col">
-            <div class="img-container mb-4">
-              <img :src="'/images/contacts/' +contact.picture" class="rounded w-full" />
-            </div>
-          </div>
+        <div id="user-data" v-if="contact">
+            <vx-card title="Contact" class="mb-base">
+                <!-- Avatar -->
+                <div class="vx-row">
+                    <!-- Avatar Col -->
+                    <div class="vx-col" id="avatar-col">
+                        <div class="img-container mb-4">
+                            <img
+                                :src="'/images/contacts/' + contact.picture"
+                                class="rounded w-full"
+                            />
+                        </div>
+                    </div>
 
-          <!-- Information - Col 1 -->
-          <div class="vx-col flex-1" id="account-info-col-1">
-            <table>
-              <tr>
-                <td class="font-semibold">Nom</td>
-                <td>{{ contact.name }}</td>
-              </tr>
-              <tr>
-                <td class="font-semibold">Email</td>
-                <td>{{ contact.email }}</td>
-              </tr>
-              <tr>
-                <td class="font-semibold">Mobile</td>
-                <td>{{ contact.mobile }}</td>
-              </tr>
-            </table>
-          </div>
-          <!-- /Information - Col 1 -->
+                    <!-- Information - Col 1 -->
+                    <div class="vx-col flex-1" id="account-info-col-1">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">Nom</td>
+                                <td>{{ contact.name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Email</td>
+                                <td>{{ contact.email }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Mobile</td>
+                                <td>{{ contact.mobile }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <!-- /Information - Col 1 -->
 
-          <!-- Information - Col 2 -->
-          <div class="vx-col flex-1" id="account-info-col-2">
-            <table>
-              <tr>
-                <td class="font-semibold">Pays</td>
-                <td>{{ contact.country }}</td>
-              </tr>
-              <tr>
-                <td class="font-semibold">State</td>
-                <td>{{ contact.state }}</td>
-              </tr>
-              <tr>
-                <td class="font-semibold">Titre de travail</td>
-                <td>{{ contact.title }}</td>
-              </tr>
-            </table>
-          </div>
-          <!-- /Information - Col 2 -->
-          <div class="vx-col w-full flex" id="account-manage-buttons">
-            <vs-button
-              icon-pack="feather"
-              icon="icon-edit"
-              class="mr-4"
-              :to="{
+                    <!-- Information - Col 2 -->
+                    <div class="vx-col flex-1" id="account-info-col-2">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">Pays</td>
+                                <td>{{ contact.country.label }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">State</td>
+                                <td>{{ contact.state }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Titre de travail</td>
+                                <td>{{ contact.title }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <!-- /Information - Col 2 -->
+                    <div class="vx-col w-full flex" id="account-manage-buttons">
+                        <vs-button
+                            icon-pack="feather"
+                            icon="icon-edit"
+                            class="mr-4"
+                            :to="{
                                 name: 'contact-edit',
                                 params: { slug: $route.params.slug }
                             }"
-            >Edit</vs-button>
-            <vs-button
-              type="border"
-              color="danger"
-              icon-pack="feather"
-              icon="icon-trash"
-              @click="confirmDeleteRecord"
-            >Delete</vs-button>
-          </div>
-        </div>
-      </vx-card>
+                            >Edit</vs-button
+                        >
+                        <vs-button
+                            type="border"
+                            color="danger"
+                            icon-pack="feather"
+                            icon="icon-trash"
+                            @click="confirmDeleteRecord"
+                            >Delete</vs-button
+                        >
+                    </div>
+                </div>
+            </vx-card>
 
-      <div class="vx-row">
-        <div class="vx-col lg:w-1/2 w-full">
-          <vx-card title="Entreprise(s)" class="mb-base">
-            <vs-chip
-              v-for="(entreprise,index) in contact.entreprises"
-              :key="index"
-            >{{entreprise.name}}</vs-chip>
-          </vx-card>
-        </div>
+            <div class="vx-row">
+                <div class="vx-col lg:w-1/2 w-full">
+                    <vx-card title="Entreprise(s)" class="mb-base">
+                        <vs-chip
+                            v-for="(entreprise, index) in contact.entreprises"
+                            :key="index"
+                            >{{ entreprise.name }}</vs-chip
+                        >
+                    </vx-card>
+                </div>
 
-        <div class="vx-col lg:w-1/2 w-full">
-          <vx-card title="Social Links" class="mb-base">
-            <table>
-              <tr>
-                <td class="font-semibold">Twitter</td>
-                <td>{{ contact.social.twitter }}</td>
-              </tr>
-              <tr>
-                <td class="font-semibold">LinkedIn</td>
-                <td>{{ contact.social.linkedin }}</td>
-              </tr>
-              <tr>
-                <td class="font-semibold">Instagram</td>
-                <td>{{ contact.social.instagram }}</td>
-              </tr>
-            </table>
-          </vx-card>
+                <div class="vx-col lg:w-1/2 w-full">
+                    <vx-card title="Social Links" class="mb-base">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">Twitter</td>
+                                <td>{{ contact.social.twitter }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">LinkedIn</td>
+                                <td>{{ contact.social.linkedin }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Instagram</td>
+                                <td>{{ contact.social.instagram }}</td>
+                            </tr>
+                        </table>
+                    </vx-card>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      contact: null,
-      user_not_found: false,
-    };
-  },
-  methods: {
-    confirmDeleteRecord() {
-      this.$vs.dialog({
-        type: "confirm",
-        color: "danger",
-        title: "Confirm Delete",
-        text: `Vous ètes entrain de suprimer "${this.contact.name}"`,
-        accept: this.deleteRecord,
-        acceptText: "Delete",
-      });
+    data() {
+        return {
+            contact: null,
+            user_not_found: false
+        };
     },
-    deleteRecord() {
-      /* Below two lines are just for demo purpose */
-      this.$router.push("/contacts");
-      this.showDeleteSuccess();
+    methods: {
+        confirmDeleteRecord() {
+            this.$vs.dialog({
+                type: "confirm",
+                color: "danger",
+                title: "Confirm Delete",
+                text: `Vous ètes entrain de suprimer "${this.contact.name}"`,
+                accept: this.deleteRecord,
+                acceptText: "Delete"
+            });
+        },
+        deleteRecord() {
+            /* Below two lines are just for demo purpose */
+            this.$router.push("/contacts");
+            this.showDeleteSuccess();
 
-      /* UnComment below lines for enabling true flow if deleting user */
-      // this.$store.dispatch("userManagement/removeRecord", this.user_data.id)
-      //   .then(()   => { this.$router.push({name:'app-user-list'}); this.showDeleteSuccess() })
-      //   .catch(err => { console.error(err)       })
-    },
-    showDeleteSuccess() {
-      this.$vs.notify({
-        color: "success",
-        title: "Contact Deleted",
-        text: "The selected contact was successfully deleted",
-      });
-    },
-  },
-  created() {
-    const slug = this.$route.params.slug;
-    this.$http
-      .get("/api/contacts/" + slug)
-      .then((res) => {
-        this.contact = res.data;
-      })
-      .catch((err) => {
-        if (err.response.status === 404) {
-          this.user_not_found = true;
-          return;
+            /* UnComment below lines for enabling true flow if deleting user */
+            // this.$store.dispatch("userManagement/removeRecord", this.user_data.id)
+            //   .then(()   => { this.$router.push({name:'app-user-list'}); this.showDeleteSuccess() })
+            //   .catch(err => { console.error(err)       })
+        },
+        showDeleteSuccess() {
+            this.$vs.notify({
+                color: "success",
+                title: "Contact Deleted",
+                text: "The selected contact was successfully deleted"
+            });
         }
-        console.error(err.response.data);
-      });
-  },
+    },
+    created() {
+        const slug = this.$route.params.slug;
+        this.$http
+            .get("/api/contacts/" + slug)
+            .then(res => {
+                this.contact = res.data;
+            })
+            .catch(err => {
+                if (err.response.status === 404) {
+                    this.user_not_found = true;
+                    return;
+                }
+                console.error(err.response.data);
+            });
+    }
 };
 </script>
 
 <style lang="scss">
 #avatar-col {
-  width: 10rem;
+    width: 10rem;
 }
 
 #page-user-view {
-  table {
-    td {
-      vertical-align: top;
-      min-width: 140px;
-      padding-bottom: 0.8rem;
-      word-break: break-all;
-    }
-
-    &:not(.permissions-table) {
-      td {
-        @media screen and (max-width: 370px) {
-          display: block;
+    table {
+        td {
+            vertical-align: top;
+            min-width: 140px;
+            padding-bottom: 0.8rem;
+            word-break: break-all;
         }
-      }
+
+        &:not(.permissions-table) {
+            td {
+                @media screen and (max-width: 370px) {
+                    display: block;
+                }
+            }
+        }
     }
-  }
 }
 
 // #account-info-col-1 {
@@ -209,19 +226,19 @@ export default {
 // }
 
 @media screen and (min-width: 1201px) and (max-width: 1211px),
-  only screen and (min-width: 636px) and (max-width: 991px) {
-  #account-info-col-1 {
-    width: calc(100% - 12rem) !important;
-  }
+    only screen and (min-width: 636px) and (max-width: 991px) {
+    #account-info-col-1 {
+        width: calc(100% - 12rem) !important;
+    }
 
-  // #account-manage-buttons {
-  //   width: 12rem !important;
-  //   flex-direction: column;
+    // #account-manage-buttons {
+    //   width: 12rem !important;
+    //   flex-direction: column;
 
-  //   > button {
-  //     margin-right: 0 !important;
-  //     margin-bottom: 1rem;
-  //   }
-  // }
+    //   > button {
+    //     margin-right: 0 !important;
+    //     margin-bottom: 1rem;
+    //   }
+    // }
 }
 </style>
